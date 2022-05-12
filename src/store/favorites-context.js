f876492158ed65acacafcb90abc48548d0fbd1c7
Anastasiia Zlatieva <zlatevaanastasia@gmail.com>
2022-05-12 14:54:07 +0200

    favorite context

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,40 +1,69 @@
-import { createContext, useState, useEffect, useRef } from "react";
+import { createContext, useState, useEffect, useRef, useReducer } from "react";
 
+const initialState = [];
 const FavoriteContext = createContext({
-  favorites: [],
+  favorites: initialState,
   addFavorite: (favoriteJoke) => {},
   removeFavorite: (id) => {},
   itemIsFavorite: (id) => {},
 });
-const initialFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+// const initialFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return [...state, action.favoriteJoke];
+    case "delete":
+      return state.filter((joke) => joke.id !== action.id);
+    case "isFavorite":
+      return Array(state).some((joke) => joke.id === action.id);
+    default:
+      return state;
+  }
+}
 
 export function FavoritesContextProvider(props) {
-  const [favorites, setFavorites] = useState(initialFavorites);
-  const isInitialState = useRef(true);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(() => {
-    if (isInitialState.current) {
-      isInitialState.current = false;
-    } else {
-      localStorage.setItem("favorites", JSON.stringify(favorites));
-    }
-  }, [favorites]);
+  // const [favorites, setFavorites] = useState(initialFavorites);
+  // const isInitialState = useRef(true);
 
-  function addFavorites(favoriteJoke) {
-    const favoriteJokes = [...favorites, favoriteJoke];
-    setFavorites(favoriteJokes);
-  }
+  // useEffect(() => {
+  //   if (isInitialState.current) {
+  //     isInitialState.current = false;
+  //   } else {
+  //     localStorage.setItem("favorites", JSON.stringify(favorites));
+  //   }
+  // }, [favorites]);
 
-  function removeFavorite(id) {
-    const favoriteJokes = favorites.filter((joke) => joke.id !== id);
-    setFavorites(favoriteJokes);
-  }
+  // function addFavorites(favoriteJoke) {
+  //   const favoriteJokes = [...favorites, favoriteJoke];
+  //   setFavorites(favoriteJokes);
+  // }
 
-  function itemIsFavorite(id) {
-    return favorites.some((joke) => joke.id === id);
-  }
+  const addFavorites = (favoriteJoke) => {
+    dispatch({ type: "add", favoriteJoke: favoriteJoke });
+  };
+
+  // function removeFavorite(id) {
+  //   const favoriteJokes = favorites.filter((joke) => joke.id !== id);
+  //   setFavorites(favoriteJokes);
+  // }
+
+  const removeFavorite = (id) => {
+    dispatch({ type: "delete", id: id });
+  };
+
+  // function itemIsFavorite(id) {
+  //   return favorites.some((joke) => joke.id === id);
+  // }
+
+  const itemIsFavorite = (id) => {
+    dispatch({ type: "isFavorite", id: id });
+  };
   const context = {
-    favorites: favorites,
+    favorites: state,
     addFavorite: addFavorites,
     removeFavorite: removeFavorite,
     itemIsFavorite: itemIsFavorite,
